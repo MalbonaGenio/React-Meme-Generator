e22516fd33441ad5a54c98168bca750cc63d23b4
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-25 13:35:17 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React from "react";
+import Header from "./Header.js";
+import MemeGenerator from "./MemeGenerator.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <h1>meme section</h1>;
+  }
+}
+
+export default MemeGenerator;
