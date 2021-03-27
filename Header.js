03e46afe5b6983288cafb5f58813d02f0686ee76
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-27 10:15:22 +0900

    added from and event hanlder added a form for the top and bottom text, added the event handler to uptade the state of those fields

--- Header.js
+++ Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <img src={require("/img/troll-face.png")} alt="Problem?" />
+      <img src={require("./img/troll-face.png")} alt="Problem?" />
       <p>Meme Generator</p>
     </header>
   );
--- MemeGenerator.js
+++ MemeGenerator.js
@@ -3,11 +3,58 @@ import React, { Component } from "react";
 class MemeGenerator extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImage: "http://i.imgflip.com/1bij.jpg",
+      allImgMemes: [],
+      topText: ""
+    };
+    this.eventHandler = this.eventHandler.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        // store the meme object in data to state
+        const { memes } = response.data;
+        this.setState({ allImgMemes: memes });
+      });
+  }
+
+  /**
+   * Create the onChagne handler method
+   * It should update the corresponding state on every change of the input box
+   */
+
+  eventHandler(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
-    return <h1>MEME GENERATOR SECTION</h1>;
+    return (
+      <div>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            placeholder="Top text"
+            onChange={this.eventHandler}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            placeholder="Bottom text"
+            onChange={this.eventHandler}
+          />
+          <button>Gen</button>
+        </form>
+      </div>
+    );
   }
 }
 
